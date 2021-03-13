da8af09ab71785b8b14800229a3083da918a27a5
tommyG96 <t.gaerner@gmail.com>
2021-03-13 21:42:13 +0100

    init

---
+++ BlazorServer/Models/FilmModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServer.Models
+{
+    public class FilmModel
+    {
+        public int film_Id { get; set; }
+        public string film_Titel { get; set; }
+        public DateTime film_Datum { get; set; }
+        public int prod_Id { get; set; }
+        public string prod_Bezeichnung { get; set; }
+        public int schau_Id { get; set; }
+        public string schau_Vname { get; set; }
+        public string schau_Nname { get; set; }
+
+        public string Schauspieler{ get; set; }
+
+    }
+}
